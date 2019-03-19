a9b01057d7f0988c3ef47b34a0fba738451df427
Dangiz <dangizzz97@gmail.com>
2019-03-19 21:38:49 +0400

    Puasson random sequences

--- 2_RandValTasks/Program.cs
+++ 2_RandValTasks/Program.cs
@@ -10,7 +10,18 @@ namespace RandValTasks
     {
         static void Main(string[] args)
         {
-            Bombardment bombardment = new Bombardment(6,60,150,60,30, new Random());
+            DateTime startMoment=new DateTime(2019,3,19,21,35,00);
+            var generator=new PuassonStreamSequenceGenerator(1);
+            for (int i = 0; i < 1000; i++)
+            {
+                startMoment=startMoment.AddMinutes(generator.GetNextTimeMoment());
+                Console.WriteLine(startMoment);
+            }
+        }
+
+        private static void BombsAway()
+        {
+            Bombardment bombardment = new Bombardment(6, 60, 150, 60, 30, new Random());
             int hits = 0;
             for (int i = 0; i < 1000; i++)
                 hits += bombardment.BombsDropping();
---
+++ 2_RandValTasks/PuassonStreamSequenceGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandValTasks
+{
+    public class PuassonStreamSequenceGenerator
+    {
+        public PuassonStreamSequenceGenerator(Random random, double lambda)
+        {
+            this.random = random;
+            Lambda = lambda;
+            lastTImeMoment = 0;
+        }
+
+        public PuassonStreamSequenceGenerator(double lambda)
+        {
+            Lambda = lambda;
+            random = new Random();
+            lastTImeMoment = 0;
+        }
+
+        private Random random { get; set; }
+        public double Lambda { get; set; }
+        private double lastTImeMoment { get; set; }
+
+        public double GetNextTimeMoment()
+        {
+            lastTImeMoment-= 1 / Lambda * Math.Log(random.NextDouble());
+            return lastTImeMoment;
+        }
+
+
+    }
+}
